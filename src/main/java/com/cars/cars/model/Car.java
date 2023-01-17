580b7c99f39686bde545e37abcf56b7cb09028bf
William <william.fujimori@gmail.com>
2023-01-17 17:01:12 -0300

    feat: add car model

---
+++ src/main/java/com/cars/cars/model/Car.java
@@ -0,0 +1,44 @@
+package com.cars.cars.model;
+
+import com.cars.cars.dto.CarsDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Car {
+
+    public Car(CarsDto data) {
+        this.modelo = data.modelo();
+        this.fabricante = data.fabricante();
+        this.dataFabricacao = data.dataFabricacao();
+        this.valor = data.valor();
+        this.anoModelo = data.anoModelo();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String modelo;
+
+    @Column(length = 50, nullable = false)
+    private String fabricante;
+
+    @Column(length = 50, nullable = false)
+    private String dataFabricacao;
+
+    @Column(nullable = false)
+    double valor;
+
+    @Column(nullable = false)
+    int anoModelo;
+}
