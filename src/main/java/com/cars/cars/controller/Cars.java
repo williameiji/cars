6d8b4cd0e1c9969715a56898a030792c54f826f7
William <william.fujimori@gmail.com>
2023-01-17 11:41:26 -0300

    feat: add new car route

---
+++ src/main/java/com/cars/cars/controller/Cars.java
@@ -0,0 +1,22 @@
+package com.cars.cars.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cars.cars.dto.CarsDto;
+
+@RestController
+@RequestMapping("/api/cars")
+public class Cars {
+
+    @PostMapping
+    public void addCars(@RequestBody CarsDto req) {
+        System.out.println(req.modelo());
+        System.out.println(req.fabricante());
+        System.out.println(req.dataFabricacao());
+        System.out.println(req.valor());
+        System.out.println(req.anoModelo());
+    }
+}
