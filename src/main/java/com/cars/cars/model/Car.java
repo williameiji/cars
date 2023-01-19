9c3252798d8af4f2bf966cf004424db5279e27c9
William <william.fujimori@gmail.com>
2023-01-19 17:16:31 -0300

    fix: type of dataFabricacao

--- src/main/java/com/cars/cars/model/Car.java
+++ src/main/java/com/cars/cars/model/Car.java
@@ -1,5 +1,7 @@
 package com.cars.cars.model;
 
+import java.util.Date;
+
 import com.cars.cars.dto.CarsDto;
 
 import jakarta.persistence.Column;
@@ -34,7 +36,7 @@ public class Car {
     private String fabricante;
 
     @Column(length = 50, nullable = false)
-    private String dataFabricacao;
+    private Date dataFabricacao;
 
     @Column(nullable = false)
     double valor;
