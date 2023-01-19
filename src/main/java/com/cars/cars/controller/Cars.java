af2c5d12a00aa4530f2a06c8d4ad6fe0e91ec7eb
William <william.fujimori@gmail.com>
2023-01-19 17:27:11 -0300

    feat: add update route

--- src/main/java/com/cars/cars/controller/Cars.java
+++ src/main/java/com/cars/cars/controller/Cars.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,18 @@ public class Cars {
     @DeleteMapping("/{id}")
     public void deleteCar(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCar(@PathVariable Long id, @RequestBody @Valid CarsDto req) {
+        repository.findById(id).map(car -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+            car.setAnoModelo(req.anoModelo());
+
+            return repository.save(car);
+        });
     }
 }
