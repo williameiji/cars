7e7e8f49ada00a3d84e05ba9327c481b973a3d1c
William <william.fujimori@gmail.com>
2023-01-19 17:18:00 -0300

    feat: add notation Valid to addCars

--- src/main/java/com/cars/cars/controller/Cars.java
+++ src/main/java/com/cars/cars/controller/Cars.java
@@ -3,7 +3,9 @@ package com.cars.cars.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cars.cars.dto.CarsDto;
 import com.cars.cars.model.Car;
 import com.cars.cars.repository.CarRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -26,7 +30,7 @@ public class Cars {
     }
 
     @PostMapping
-    public void addCars(@RequestBody CarsDto req) {
+    public void addCars(@RequestBody @Valid CarsDto req) {
         repository.save(new Car(req));
     }
 }
