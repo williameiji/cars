f88f65d9943bd2dec6e8b647747fd65a0296f531
William <william.fujimori@gmail.com>
2023-01-19 17:15:43 -0300

    feat: add attribute validation

--- src/main/java/com/cars/cars/dto/CarsDto.java
+++ src/main/java/com/cars/cars/dto/CarsDto.java
@@ -1,8 +1,16 @@
 package com.cars.cars.dto;
 
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
 public record CarsDto(
-        String modelo, String fabricante,
-        String dataFabricacao,
-        double valor, int anoModelo) {
+                @NotBlank String modelo,
+                @NotBlank String fabricante,
+                @Past Date dataFabricacao,
+                @NotNull double valor,
+                @NotNull int anoModelo) {
 
 }
